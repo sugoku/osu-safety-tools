3eb1b476ef81ae0527ccbf9986bc725fcee251c7
sugoku <47049597+sugoku@users.noreply.github.com>
2021-07-04 10:48:29 -0400

    Speed up audio generation for single BPM songs

--- osu_audio_gen.py
+++ osu_audio_gen.py
@@ -7,7 +7,7 @@ from pydub import AudioSegment
 def generate_audio(osufn, progress=None):
     osufn = Path(osufn)
 
-    global_offset = 18  # milliseconds
+    global_offset = 13  # milliseconds
 
     audiofn = "audio.mp3"
 
@@ -49,42 +49,75 @@ def generate_audio(osufn, progress=None):
     # offset in ms, beat length, beats per measure, sound set, idk, volume, inherited, flags
     # generate beat timestamps
     l = len(timing_points)
-    for i in range(len(timing_points)):
-        if progress is not None:
-            progress.emit(int(i/l*10))
-        offset = float(timing_points[i][0])
-        next_offset = float(timing_points[i+1][0]) if i < len(timing_points)-1 else len(audio)
-        beat_length = float(timing_points[i][1])
-        beats_per_measure = int(timing_points[i][2])
+    if l == 1:
+        # for single BPM songs you could just do 
+        offset = float(timing_points[0][0])
+        beat_length = float(timing_points[0][1])
+        beats_per_measure = int(timing_points[0][2])
+        
+        loop = AudioSegment.silent(duration=beat_length*beats_per_measure)
+        
+        progress.emit(10)
         
         if beats_per_measure == 4:
-            kicks += list(np.arange(offset + global_offset, next_offset + global_offset, beat_length*2))
-            hihats += list(np.arange(offset + beat_length/2 + global_offset, next_offset + global_offset, beat_length))
-            claps += list(np.arange(offset + beat_length + global_offset, next_offset + global_offset, beat_length*2))
+            loop = loop.overlay(kick, position=0).overlay(hihat, position=beat_length).overlay(clap, position=beat_length*2).overlay(hihat, position=beat_length*3)
         elif beats_per_measure == 2:
-            kicks += list(np.arange(offset + global_offset, next_offset + global_offset, beat_length))
-            hihats += list(np.arange(offset + beat_length/2 + global_offset, next_offset + global_offset, beat_length))
+            loop = loop.overlay(kick, position=0).overlay(hihat, position=beat_length)
         else:
-            kicks += list(np.arange(offset + global_offset, next_offset + global_offset, beat_length*beats_per_measure))
-            hihats += list(np.arange(offset + beat_length + global_offset, next_offset + global_offset, beat_length))
-    
-    i = 0
-    l = len(kicks)+len(hihats)+len(claps)
-    for pos in kicks:
-        audio = audio.overlay(kick, position=int(pos))
-        if progress is not None:
-            progress.emit(10+int(i/l*90))
-        i += 1
-    for pos in hihats:
-        audio = audio.overlay(hihat, position=int(pos))
-        if progress is not None:
-            progress.emit(10+int(i/l*90))
-        i += 1
-    for pos in claps:
-        audio = audio.overlay(clap, position=int(pos))
-        if progress is not None:
-            progress.emit(10+int(i/l*90))
-        i += 1
+            loop = loop.overlay(kick, position=0)
+            for i in range(beats_per_measure - 1):
+                loop = loop.overlay(hihat, position=beat_length*(i+1))
+                
+        progress.emit(40)
+            
+        loop *= int(len(audio) / (beat_length*beats_per_measure) + 1)
+        
+        progress.emit(80)
+            
+        audio = audio.overlay(loop, position=global_offset+offset)
+        
+        progress.emit(99)
+    else:
+        for i in range(len(timing_points)):
+            if progress is not None:
+                progress.emit(int(i/l*10))
+            offset = float(timing_points[i][0])
+            next_offset = float(timing_points[i+1][0]) if i < len(timing_points)-1 else len(audio)
+            beat_length = float(timing_points[i][1])
+            beats_per_measure = int(timing_points[i][2])
+            
+            # consider speeding up by pre-rendering kick-hat-clap-hat pattern for each timing_points' beat_length
+            # and then just overlaying the patterns up until next_offset and then cut leftover audio after next_offset
+            # consequentially this makes BPM changes sound more abrupt but it's way faster in theory and removes the external for loops
+            
+            if beats_per_measure == 4:
+                kicks += list(np.arange(offset + global_offset, next_offset + global_offset, beat_length*2))
+                hihats += list(np.arange(offset + beat_length/2 + global_offset, next_offset + global_offset, beat_length))
+                claps += list(np.arange(offset + beat_length + global_offset, next_offset + global_offset, beat_length*2))
+            elif beats_per_measure == 2:
+                kicks += list(np.arange(offset + global_offset, next_offset + global_offset, beat_length))
+                hihats += list(np.arange(offset + beat_length/2 + global_offset, next_offset + global_offset, beat_length))
+            else:
+                kicks += list(np.arange(offset + global_offset, next_offset + global_offset, beat_length*beats_per_measure))
+                hihats += list(np.arange(offset + beat_length + global_offset, next_offset + global_offset, beat_length))
+                
+        i = 0
+        l = len(kicks)+len(hihats)+len(claps)
+        for pos in kicks:
+            audio = audio.overlay(kick, position=int(pos))
+            if progress is not None:
+                progress.emit(10+int(i/l*90))
+            i += 1
+        for pos in hihats:
+            audio = audio.overlay(hihat, position=int(pos))
+            if progress is not None:
+                progress.emit(10+int(i/l*90))
+            i += 1
+        for pos in claps:
+            audio = audio.overlay(clap, position=int(pos))
+            if progress is not None:
+                progress.emit(10+int(i/l*90))
+            i += 1
 
     os.rename(audiofn, f"{audiofn}.bak")
     audio.export(audiofn)
